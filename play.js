1ca445849a7301ca9c4f9c8721ece8fadd812716
Ian Sebleius <sebeliusancira@gmail.com>
2023-06-15 17:25:25 -0700

    refactor: create input.js and export setupinput into play.js

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const {connect} = require('./client.js');
 
+const {setupInput} = require('./input.js');
+
 console.log('Connecting ...');
 connect();
 
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
 setupInput();
-
-// module.exports = setupInput;
